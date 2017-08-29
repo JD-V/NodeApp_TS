031a82b93149de616a621c1c7b0e8b9d46b63c00
Jaydeep Vaghasiya <vaghasiyajaydeep@gmail.com>
2017-08-29 19:53:22 +0530

    body-parser in effect

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 'use strict'
 
 var express = require('express');
+var jsonParser = require('body-parser').json;   
+// body parser contains many parsers underneath 
+//to parser different kind of http request.
+// we need json parser.
 
 var app = express();
 
+var jsoncheck = function(req,res,next){
+    if(req.body) {
+        console.log("The sky is blue");
+    } else {
+        console.log("There is no body property in request.")
+    }
+    next()
+}
+
+app.use(jsoncheck);
+app.use(jsonParser()); 
+app.use(jsoncheck);
+// json middleware
+// when the app receives a request this middleware 
+//will parse the request and make it available to 
+//next middleware as json object
+
 app.use(function(req,res,next){
-    req.myMessage = "Hello from Middleware 1"
-    console.log("The leaves on the tress are " + req.query.color)
-    next(); //handover the response to next middleware
+    if(req.body) {
+        console.log("The sky is blue");
+    } else {
+        console.lof("There is no body property in request.")
+    }
+    next()
 });
 
 
