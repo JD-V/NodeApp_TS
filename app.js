b353a0c2495662f9eab9af0820bacfc567e15163
Jaydeep Vaghasiya <vaghasiyajaydeep@gmail.com>
2017-08-29 19:31:54 +0530

    Basic usage of middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var express = require('express');
 
 var app = express();
 
+app.use(function(req,res,next){
+    console.log("First piece of middleware")
+    next(); //handover the response to next middleware
+});
+
+app.use("/different/:id",function(req,res,next){
+    console.log("Second piece of middleware, ID " + req.params.id)
+    next(); //handover the response to next middleware
+});
+
 var port = process.env.port || 3002;
 
 app.listen(port, function(){
