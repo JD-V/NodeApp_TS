d4446ef1a10c13b8d114d80ce4d899fe454229a0
Jaydeep Vaghasiya <vaghasiyajaydeep@gmail.com>
2017-08-30 22:11:08 +0530

    using Schema.create and pre hook

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -18,11 +18,25 @@ db.once("open", function() {
     var Schema = mongoose.Schema;
     var AnimalSchema = new Schema({
         type: {type:String, default:"goldFish"},
-        color: {type:String, default:"golden"},
+        color: String,
         size: {type:String, default:"small"},
         mass: {type:Number, default:0.007},
         name: {type:String, default:"Angela"}
     });
+
+
+    // Pre-hook this handler gets executed before Mongo executes save
+    AnimalSchema.pre("save", function(next){
+        if(this.mass >= 100) {
+            this.size = 'big';
+        } else if(this.mass >=5 && this.mass<=100) {
+            this.size = 'medium';
+        } else {
+            this.size = 'small'; 
+        }
+
+        next();
+    })
 
     // Create a model named Animal
     // This will map to a collection in mongo DB database
@@ -30,7 +44,6 @@ db.once("open", function() {
 
     var elephant = new Animal({
         type: 'elephant',
-        size: 'big',
         color: 'gray',
         mass: 6000,
         name: 'Lawrancec'
@@ -41,35 +54,52 @@ db.once("open", function() {
     //color not supplied should pick up the default
     var whale = new Animal({   
         type:'Whale',
-        size:'big',
         mass:190500,
         name:'Fig'
     });
 
+    var animalData = [
+        {
+            type: "mouse",
+            color: "gray",
+            mass: 0.035,
+            name:"Marvin" 
+        },
+        {
+            type: "nutria",
+            color: "browne",
+            mass: 6.35,
+            name:"Gretchen" 
+        },
+        {
+            type: "wolf",
+            color: "gray",
+            mass: 45,
+            name:"Iris" 
+        },
+        elephant,
+        animal,
+        whale
+    ]
+
      // remove without any condition , will remove all the documents
     Animal.remove({},function(err) {
-        
-        elephant.save(function(err) {
+        Animal.create(animalData,function(err, animals){
             if(err) console.log("Save Failed", err);
-            
-            animal.save(function(err) {
-                if(err) console.log("Save Failed", err);
+            Animal.find({}, function(err, animals){
+                animals.forEach(function(animal){
+                    console.log(animal.name + " the " + animal.color + " " + animal.type + " is a " + animal.size + "-sized" );
+                });
 
-                whale.save(function(err) {
-                    if(err) console.log("Save Failed", err);
-
-                    Animal.find({"size" : "big"}, function(err, animals) {
-                        animals.forEach(function(animal){
-                            console.log(animal.name + " the " + animal.color + " " + animal.type );
-                        });
-
-                        db.close(function() {
-                            //close the connection  
-                            console.log("connectin is closed");
-                        }); 
-                    });
-                });          
+                db.close(function() {
+                    //close the connection  
+                    console.log("connectin is closed");
+                });
             });
-        });
+
+            
+
+        });    
     });
-})
+
+});
