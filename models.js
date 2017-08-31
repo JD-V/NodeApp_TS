5a7ee7064a3e60b9349eadb2a999f5d16f0ca330
Jaydeep Vaghasiya <vaghasiyajaydeep@gmail.com>
2017-09-01 00:25:50 +0530

    model with pre hook and instance method

--- models.js
+++ models.js
@@ -4,6 +4,20 @@ var mongoose = require('nongoose');
 
 var schema = mongoose.Schema;   //schema constructor
 
+//custom sort logic to sort first based on number of votes and then updatedAt time
+var sortAnswers = function(a,b) {
+    //-ve if a before b
+    // 0 if no change
+    //+ve if a after b
+
+    // sort answers by vote
+    
+    if(b.votes === a.votes) {
+       return b.updatedAt-a.updatedAt;//returns milli-seconds.
+    }
+    return b.votes-a.votes;
+}
+
 var AnswerSchema = new Schema({
     text:string,
     createdAt: {Date},  //using default value will replace value of createdAt with current date time.
@@ -12,10 +26,49 @@ var AnswerSchema = new Schema({
 
 });
 
+// creating instance method (another way to create it)
+// notice one thing, mongooese alerady has save method
+// so we have named this as update method otherwise it
+// will adversly affect save operation
+AnswerSchema.methods("update", function(updates,callback){
+    //this == document
+    // Object.assign(target, ...sources)
+    // Properties in the target object will be overwritten by properties 
+    // in the sources if they have the same key.  
+    // Later sources' properties will similarly overwrite earlier ones.  
+    object.assign(this, updates, {updatedAt: Date.now});
+
+    this.parent().save(callback);
+})
+
+
+// Q. What's the easiest way to get a reference to a parent document in mongoose if you have its child?
+// A. call the child document's parent() method.
+
+AnswerSchema.methods("votes", function(updates,callback){
+   if(votes == up)
+        this.votes += 1;
+    else
+        this.votes -= 1;
+
+    this.parent().save(callback);
+})
+
+
 var QuestionsSchema = new mongoose.Schema({
     text:String,
     createdAt: {type:Date, default:Date.now},  //using default value will replace value of createdAt with current date time.
     answers: [AnswerSchema]
+});
+
+
+// By using this pre save hook 
+// mongo will sort answers array every time it sorts.
+Question.pre("save", function(next) {
+    // sort() is a default javascript sort method and cannot sort objects, here anserws is obeject array [object, object]
+    // you can use your custom sortig logic as well.
+    this.answers.sort(sortAnswers); 
+    next();
 });
 
 var Question = mongoose.model("question", QuestionsSchema);
